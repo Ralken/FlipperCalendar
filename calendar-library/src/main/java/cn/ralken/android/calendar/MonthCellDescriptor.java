6ddfed7e6c562c4275f74b7fb86124f52a6e06dd
Ralken Liao <zeroyakon@gmail.com>
2017-10-22 20:05:58 +0800

    Use view descriptions that describes what elements the view(s) may have

---
+++ calendar-library/src/main/java/cn/ralken/android/calendar/MonthCellDescriptor.java
@@ -0,0 +1,75 @@
+package cn.ralken.android.calendar;
+
+import java.util.Date;
+
+/** Describes the state of a particular date cell in a {@link MonthView}. */
+class MonthCellDescriptor {
+	private final Date date;
+	private final int value;
+	private final boolean isPreviousMonth;
+	private final boolean isCurrentMonth;
+	private final boolean isNextMonth;
+	private final boolean isToday;
+	
+	private boolean isSelected;
+	private boolean hasExtrarValue = false;
+	
+	MonthCellDescriptor(Date date, boolean previousMonth, boolean currentMonth, boolean nextMonth, boolean selected,
+			boolean today, int value) {
+		this.date = date;
+		isPreviousMonth = previousMonth;
+		isCurrentMonth = currentMonth;
+		isNextMonth = nextMonth;
+		isSelected = selected;
+		isToday = today;
+		
+		this.value = value;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public boolean isPreviousMonth() {
+		return isPreviousMonth;
+	}
+
+	public boolean isNextMonth() {
+		return isNextMonth;
+	}
+
+	public boolean isCurrentMonth() {
+		return isCurrentMonth;
+	}
+
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	public void setSelected(boolean selected) {
+		isSelected = selected;
+	}
+	
+	public boolean isToday() {
+		return isToday;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	public boolean hasExtrarValue() {
+		return hasExtrarValue;
+	}
+
+	public void setHasExtrarValue(boolean hasExtrarValue) {
+		this.hasExtrarValue = hasExtrarValue;
+	}
+	
+	@Override
+	public String toString() {
+		return "MonthCellDescriptor{" + "date=" + date + ", value=" + value
+				+ ", isCurrentMonth=" + isCurrentMonth + ", isSelected="
+				+ isSelected + ", isToday=" + isToday + '}';
+	}
+}
---
+++ calendar-library/src/main/java/cn/ralken/android/calendar/MonthDescriptor.java
@@ -0,0 +1,29 @@
+package cn.ralken.android.calendar;
+
+class MonthDescriptor {
+  private final int month;
+  private final int year;
+  
+  
+  public MonthDescriptor(int month, int year) {
+    this.month = month;
+    this.year = year;
+  }
+
+  public int getMonth() {
+    return month;
+  }
+
+  public int getYear() {
+    return year;
+  }
+  
+  @Override public String toString() {
+    return "MonthDescriptor{"
+    	+ ", month="
+        + month
+        + ", year="
+        + year
+        + '}';
+  }
+}
