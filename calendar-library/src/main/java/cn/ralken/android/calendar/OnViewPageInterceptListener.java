7ad289305faacc7773975e14cdfea25a2da94bb3
Ralken Liao <zeroyakon@gmail.com>
2017-10-23 10:29:51 +0800

    ViewPageInterceptListener that intercepts the view touch events that supports your own click action

---
+++ calendar-library/src/main/java/cn/ralken/android/calendar/OnViewPageInterceptListener.java
@@ -0,0 +1,9 @@
+package cn.ralken.android.calendar;
+
+/**
+ * @author Ralken Liao
+ * @version create time：Nov 30, 2012 4:39:32 PM
+ */
+public interface OnViewPageInterceptListener {
+    void requestDisallowInterceptTouchEvent(boolean request);
+}
