376750e8fc84148b92c5776138200171ad3c2565
Ralken Liao <zeroyakon@gmail.com>
2017-11-15 18:50:15 +0800

    Append Apache License 2.0 comment of source code

--- calendar-library/src/main/java/cn/ralken/android/calendar/CalendarGridView.java
+++ calendar-library/src/main/java/cn/ralken/android/calendar/CalendarGridView.java
@@ -10,6 +10,20 @@ import android.view.ViewGroup;
 import static android.view.View.MeasureSpec.AT_MOST;
 import static android.view.View.MeasureSpec.EXACTLY;
 import static android.view.View.MeasureSpec.makeMeasureSpec;
+/** Copyright Ralken Liao
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * ViewGroup that draws a grid of calendar cells.  All children must be {@link CalendarRowView}s.
